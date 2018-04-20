31e4a43fb2c594823e8e6a366ad0a5b7843474c2
Berry  Semexan <bsemexan@gmail.com>
2018-04-20 08:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const app = "I don't do much."
 
 var destructivelyAppendKitten = (name) => (name.push());
 
-var destructivelyPrependKitten= (name) => ()
+var destructivelyPrependKitten= (name) => (name.unshift())
